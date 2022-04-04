bcd611920e9917a7d9993df685288eea0d51e44d
stefaan <stefaan@ponnet.com>
2022-04-04 18:44:29 +0200

    bump version

--- build/files/entrypoint.sh
+++ build/files/entrypoint.sh
@@ -5,6 +5,8 @@ echo "Starting NEAR node on network $NETWORK"
 # Check if this node has been initialized already
 if [ ! -f /root/.near/config.json ]; then
 
+    echo "No data found - initializing chain"
+
     # Initialize chain
     NEARD_INIT="neard init --chain-id $NETWORK --download-genesis"
     echo "No config found - initializing node: $NEARD_INIT"
@@ -13,12 +15,24 @@ if [ ! -f /root/.near/config.json ]; then
     # Download a snapshot to sync faster
     echo "Downloading chain snapshot"
 
-    SNAPSHOT_URL="https://near-protocol-public.s3.ca-central-1.amazonaws.com/backups/$NETWORK/rpc/data.tar"
-    echo "Retrieving snapshot from $SNAPSHOT_URL"
+    LATEST=`/app/s5cmd --no-sign-request=true cat s3://near-protocol-public/backups/$NETWORK/archive/latest`
+    if [ -z "$LATEST" ]; then
+        echo "ERROR: cannot find latest snapshot tag.. Waiting 1 hour to retry"
+        date
+        sleep 3600 
+        exit
+    fi
+
+    SNAPSHOT_BUCKET="s3://near-protocol-public/backups/$NETWORK/archive/$LATEST/*"
+    echo "Latest snapshot at: $SNAPSHOT_BUCKET"
+
+    echo "Retrieving snapshot from $SNAPSHOT_BUCKET"
     mkdir -p /root/.near/data
     cd /root/.near/data
-    wget -qO- $SNAPSHOT_URL | tar xv
+    /app/s5cmd --no-sign-request=true cp $SNAPSHOT_BUCKET .
     echo "Download of snapshot complete."
+
+
 fi
 
 ls -l /root/.near
